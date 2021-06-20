cb940f66e7678397f5d49ccc226d4c279cbd49dd
rocknrold <43779189+rocknrold@users.noreply.github.com>
2021-06-20 10:41:53 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,8 @@ const Header = () => {
     return (
         <div className="header-player">
             <Typography variant="h4" align="center" gutterBottom >
-                <b>WEARyGASM</b>
+                {/*<b>WEARyGASM</b>*/}
+                <b>Music on Repeat</b>
                 <Icon style={{ fontSize: 30 }}>headset</Icon>
             </Typography>
             <Typography variant="caption" display="block" gutterBottom align="center">
